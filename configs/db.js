5ced37c96a0c6d4c15a0c62e01738816a9991483
Ishan Rastogi <ishanrastogi15@gmail.com>
2023-01-03 13:45:55 +0100

    prettify configs

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose');
-const config = require('./config');
+import mongoose from "mongoose";
+import config from "./config"
+
 const { db: { host, port, name } } = config;
 const connectionString = `mongodb://${host}:${port}/${name}`;
-
 mongoose.connect(connectionString);
+
+mongoose.connection.on("error", console.error.bind(console, "MongoDB connection error:"));
