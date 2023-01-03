da2a677af34089927edc1b08b98c72feb3e8ea04
Ishan Rastogi <ishanrastogi15@gmail.com>
2023-01-03 17:21:56 +0100

    sample routes

--- routes/characterRouter.js
+++ routes/characterRouter.js
@@ -1,4 +1,17 @@
 import express from "express";
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.send("CATCH ALL");
+});
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const result = true
+    if (!result){
+        return res.status(400).send("Oops...Didn't see that one coming");
+    }
+    res.send(result);
+});
+
 export { router as characterRouter };
